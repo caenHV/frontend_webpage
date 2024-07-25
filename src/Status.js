c3db5d2891dd946f0fb0566025caf09f3bc91095
nikita-p <nikita.petrov.1997@gmail.com>
2024-07-25 12:03:22 +0700

    Add Interlock bagde template

--- src/Status.js
+++ src/Status.js
@@ -6,33 +6,41 @@ const { origin } = myConfig[process.env.REACT_APP_CAEN];
 const { updatetime } = myConfig[process.env.REACT_APP_CAEN].status;
 
 function StatusBadge({ name, apiroute }) {
-    const [status, setStatus] = useState(true);
+    const [badgestatus, setStatus] = useState('good');
+    const badgecomments = {
+        good: "OK",
+        failed: "Failed",
+        warning: "..."
+    };
 
     useEffect(() => {
         const interval = setInterval(() => {
             fetch(`${origin}/${apiroute}/status`)
                 .then(response => {
-                    setStatus(() => response.ok);
+                    setStatus(() => (response.ok ? 'good' : 'failed'));
                 });
         }, updatetime);
         return () => clearInterval(interval);
     }, [apiroute]);
 
-    const badgestatus = status ? 'badgegood' : 'badgefailed';
-    const badgecomment = status ? 'OK' : 'Failed';
-
+    const badgecomment = `${name}: ${badgecomments[badgestatus]}`;
     return (
-        <div className='badgeBlock'>
-            <span className={badgestatus}>{name}: {badgecomment}</span>
-        </div>);
+        <StatusBadgeDiv title={badgecomment} badgestatus={badgestatus} />
+    );
+}
 
+function StatusBadgeDiv({ title, badgestatus }) {
+    return (<div>
+        <span className={["badge", badgestatus].join(' ')}>{title}</span>
+    </div>);
 }
 
 export function StatusBlock() {
     return (
         <div className="statusbody">
             <StatusBadge name="Device" apiroute="device_backend" />
-            <StatusBadge name="Monitor" apiroute="monitor" good={false} />
+            <StatusBadge name="Monitor" apiroute="monitor" />
+            {/* <StatusBadgeDiv title="Interlock: UP" badgestatus="warning" /> */}
         </div>
     );
 }
